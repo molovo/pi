3a7d6269ee4658f3963ab5875f715b67a5ca50b7
James Dinsdale <hi@molovo.co.uk>
2016-01-24 17:30:27 +0000

    Pi is now Pug

--- app/views/home.php
+++ app/views/home.php
@@ -2,15 +2,19 @@
 <html lang='en'>
 <head>
     <meta charset='UTF-8'>
-    <title>Welcome to Pi!</title>
+    <title>Welcome to Pug!</title>
 
     <link href='https://fonts.googleapis.com/css?family=GFS+Didot&text=π' rel='stylesheet' />
     <link rel='stylesheet' href='/assets/css/main.min.css' />
 </head>
 <body>
     <header>
-        <h1>π</h1>
-        <h2>Hello World.</h2>
+        <!--
+            Icon created by Eugen Belyakoff
+            from the Noun Project
+        -->
+        <?php include PUB_ROOT.'assets/img/pug.svg' ?>
+        <h1>Hello World.</h1>
     </header>
 
     <script src='/assets/js/main.min.js'></script>
