2901099334f5e21614812a1bc9e84ba21f992822
James Dinsdale <hi@molovo.co.uk>
2016-02-02 22:34:18 +0000

    Add APP_NAMESPACE constant to index.php

--- public/index.php
+++ public/index.php
@@ -4,7 +4,8 @@ define('DS', DIRECTORY_SEPARATOR);
 define('PUB_ROOT', __DIR__.DS);
 define('BASE_ROOT', dirname(__DIR__).DS);
 define('APP_ROOT', BASE_ROOT.'app'.DS);
+define('APP_NAMESPACE', 'App\\');
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-Pi\Framework\Application::bootstrap();
+Pug\Framework\Application::bootstrap();
